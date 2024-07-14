bfcce308422cc51a5815ae7271f192181b1ff301
SeongWonKim <kssw888@gmail.com>
2024-07-14 15:09:51 +0900

    Updating LCA algorithm.

--- Basic/LCA.cpp
+++ Basic/LCA.cpp
@@ -1,73 +1,77 @@
 #include "LCA.h"
 
-int LCA::LeastCommonAnc(int left, int right) const
+size_t LCA::LeastCommonAncestor(size_t left, size_t right) const
 {
-	while (depth_table[left] != depth_table[right]) {
-		if (depth_table[left] > depth_table[right])
-			left = depth_matrix[left][static_cast<int>(std::floor(std::log2(depth_table[left] - depth_table[right])))];
-		else
-			right = depth_matrix[right][static_cast<int>(std::floor(std::log2(depth_table[right] - depth_table[left])))];
+	// First, we need to match the depth of the two nodes.
+	while (depth_table_[left] != depth_table_[right]) { // The two nodes have the same depth? If not-
+		if (depth_table_[left] > depth_table_[right]) // If the left node is deeper than the right,
+			left = parent_matrix_[left][static_cast<size_t>(std::floor(std::log2(depth_table_[left] - depth_table_[right])))]; // The left goes upstream.
+		else 
+			right = parent_matrix_[right][static_cast<size_t>(std::floor(std::log2(depth_table_[right] - depth_table_[left])))];
 	}
 
-	if (left == right) return left;
+	if (left == right) return left; // The left and right nodes are the same? Then, it's the LCA.
 
-	for (int i = static_cast<int>(std::floor(std::log2(depth_table[left]))); i >= 0; i--)
-	{
-		if (depth_matrix[left][i] != depth_matrix[right][i]) {
-			left = depth_matrix[left][i];
-			right = depth_matrix[right][i];
+	// Second, we need to search for the LCA by moving the two nodes by 2^i.
+	for (int i = static_cast<size_t>(std::floor(std::log2(depth_table_[left]))); i >= 0; i--){
+		if (parent_matrix_[left][i] != parent_matrix_[right][i]) { // If the 2^i parents are not the same,
+			//The left and right go upstream to be the 2^i parents.
+			left = parent_matrix_[left][i]; 
+			right = parent_matrix_[right][i];
 		}
 	}
 
-	return depth_matrix[left][0];
+	return parent_matrix_[left][0];
 }
 
 void LCA::SetDepth() 
 {
 	Vector visit_table(size_, 0);
-	depth_table.clear(); depth_matrix.clear();
-	depth_table.resize(size_);
-	depth_matrix.resize(size_);
-	for (auto& itr : depth_matrix)
-		itr.resize(static_cast<int>(std::log2(size_)) + 1, -1);
+	depth_table_.clear(); parent_matrix_.clear();
+	depth_table_.resize(size_);
+	parent_matrix_.resize(size_);
+	for (auto& itr : parent_matrix_)
+		itr.resize(static_cast<size_t>(std::log2(size_)) + 1, size_ + 2 );
 	visit_table[0] = 1;
-	depth_matrix[0][0] = 0;
-	SimpleDFS(visit_table, 0, 0);
-	depth_matrix[0][0] = -1;
+	parent_matrix_[0][0] = 0;
+	SetDepthUtility(visit_table, 0, 0);
 }
 
-void LCA::SimpleDFS(Vector& visit_table ,int now, int depth) 
+void LCA::SetDepthUtility(Vector& visit_table ,int now, int depth) 
 {
-	depth_table[now] = depth;
-	int temp{ 0 };
-	for (size_t i = 0; i < depth_matrix[now].size() - 1; i++)
-	{
-		temp = depth_matrix[now][i];
-		if (depth_matrix[temp][i] == -1) break;
-		else depth_matrix[now][i + 1] = depth_matrix[temp][i];
+	depth_table_[now] = depth;
+	size_t temp_parent{ size_t() };
+
+	// Recording parents
+	for (size_t i = 0; i < parent_matrix_[now].size() - 1; i++){
+		temp_parent = parent_matrix_[now][i]; // My 2^i_th parent
+		if (parent_matrix_[temp_parent][i] == size_ + 2) break; // No more parents?
+		else parent_matrix_[now][i + 1] = parent_matrix_[temp_parent][i]; // My 2^(i+1)_th parent is the parent of my 2^i_th parent.
 	}
+
+	// Recall this function for my children.
 	for (const auto& itr : matrix_[now]) {
-		if (visit_table[itr.first] == 0) {
-			visit_table[itr.first] = 1;
-			depth_matrix[itr.first][0] = now;
-			SimpleDFS(visit_table, itr.first, depth + 1);
+		if (visit_table[itr.first] == 0) { // Is it my child?
+			visit_table[itr.first] = 1; 
+			parent_matrix_[itr.first][0] = now; // I'm the 2^0_th parent of my child.
+			SetDepthUtility(visit_table, itr.first, depth + 1); // Recurrence.
 		}
 	}
 }
 
 void LCA::PrintDepthTable() const
 {
-	if (depth_table.empty()) std::cout << "Call this after SetDepth!" << std::endl;
+	if (depth_table_.empty()) std::cout << "Call this after SetDepth!" << std::endl;
 	else
-		for (const auto& itr : depth_table) std::cout << itr << ' ';
+		for (const auto& itr : depth_table_) std::cout << itr << ' ';
 	std::cout << std::endl;
 }
 
-void LCA::PrintDepthMatrix() const
+void LCA::PrintParentMatrix() const
 {
-	if (depth_table.empty()) std::cout << "Call this after SetDepth!" << std::endl;
+	if (depth_table_.empty()) std::cout << "Call this after SetDepth!" << std::endl;
 	else {
-		for (const auto& itr : depth_matrix)
+		for (const auto& itr : parent_matrix_)
 			for (const auto& itr_2 : itr)
 				std::cout << itr_2 << ' ';
 		std::cout << std::endl;
--- Basic/LCA.h
+++ Basic/LCA.h
@@ -4,28 +4,28 @@
 #include <cmath>
 
 class LCA {
-	using Vector = std::vector< int >;
+	using Vector = std::vector< int >; 
 	using Matrix = std::vector< Vector >;
-	using PairVector = std::vector< std::pair<size_t , int> >;//index, value
+	using PairVector = std::vector< std::pair<size_t , int> >; // Index and value
 	using PairMatrix = std::vector < PairVector >;
-public:
-	void Insert(const int& left, const int& right, const int& value) {
+public: // Main Class Functions
+	void Insert(const size_t& left, const size_t& right, const int& value) {
 		matrix_[left].push_back(std::make_pair(right, value)); matrix_[right].push_back(std::make_pair(left, value));
-	}
-	int LeastCommonAnc(int left, int right) const;
-	void SetDepth() ;
-	void PrintDepthTable() const;
-	void PrintDepthMatrix() const;
+	} // Insert the element to the matrix_.
+	size_t LeastCommonAncestor(size_t left, size_t right) const; // Search for the LCA of two nodes with left and right indexes.
+	void SetDepth(); // You can set the depth_table_ and parent_matrix_ to search for the least common ancestor using LeastCommonAncestor() later.
 public:
 	LCA(const size_t& size) :size_(size) {
 		matrix_.resize(size_);
 	}
 	const PairMatrix& get_matrix() const { return matrix_; }
+	void PrintDepthTable() const;
+	void PrintParentMatrix() const;
 private:
-	void SimpleDFS(Vector& visit_table, int now, int depth);
+	void SetDepthUtility(Vector& visit_table, int now, int depth); // This is a crucial utility function for SetDepth().
 private:
-	Vector depth_table;
-	Matrix depth_matrix;
-	size_t size_;
-	PairMatrix matrix_;//adj list
+	Vector depth_table_; // This table records the depth of each node.
+	Matrix parent_matrix_; // This matrix records the parents of each node. 
+	size_t size_; 
+	PairMatrix matrix_; // This matrix is an adjacent list, which has information about which nodes are conneted to which nodes.
 };
